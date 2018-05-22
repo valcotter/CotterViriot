1c499910b812d1ff1383676e13187a3ba5b79bd6
Audrey <Audrey@DESKTOP-O3OQR6H>
2018-05-22 13:14:31 +0200

    22.05.2018 -> fin formulaire contact

--- src/AppliContact/AppliContact.java
+++ src/AppliContact/AppliContact.java
@@ -13,12 +13,15 @@ import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
-
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import javafx.scene.layout.Border;
+import javafx.scene.shape.Box;
 
 public class AppliContact extends JPanel{
 	
@@ -44,27 +47,85 @@ public class AppliContact extends JPanel{
 	
 	public class FormulaireCreation extends JPanel{
 		
+		//Les différents panels 
+		private JPanel photo = new JPanel(); 
+		private JPanel formulaire = new JPanel(); 
+		private JPanel barreSuperieur = new JPanel(); 
+		
 		//Les labels du formulaire 
 		private JLabel nomL = new JLabel("Nom : "); 
-		private JLabel prenomL = new JLabel("Prénom : "); 
+		private JLabel prenomL = new JLabel("Prénom : ");
+		private JLabel numtelL = new JLabel("Numéro de téléphone : "); 
+		private JLabel mailL = new JLabel("Mail : "); 
+		private JLabel titre = new JLabel("NOM PRENOM"); 
 
 		//Les textField du formulaire 
-		private JTextField nomT = new JTextField(); 
-		private JTextField prenomT = new JTextField(); 
+		private JTextField nomT = new JTextField(20); 
+		private JTextField prenomT = new JTextField(20); 
+		private JTextField numTelT = new JTextField(12); 
+		private JTextField mailT = new JTextField(12); 
 		
-		//L'image du contact par défaut 
+		//Les images et icones 
 		private ImageIcon photoContact = new ImageIcon("contactDefaut.png"); 
 	    private JLabel phtContact = new JLabel(photoContact); 
+	    private ImageIcon retour = new ImageIcon("retour.png"); 
+	    private JLabel precedent = new JLabel(retour); 
+	    private ImageIcon edit = new ImageIcon("edit.png"); 
+	    private JLabel modifier = new JLabel(edit); 
+	    private ImageIcon poubelle = new ImageIcon("poubelle.png"); 
+	    private JLabel supprimer = new JLabel(poubelle);
+	    private ImageIcon save = new ImageIcon("save.png"); 
+	    private JLabel sauvegarder = new JLabel(save);
+	    
 				
 		public FormulaireCreation() {
 			
-			this.setLayout(new GridLayout(0, 2)); 
-			this.add(phtContact); 
-			this.add(nomL); 
-			this.add(nomT); 
-			this.add(prenomL); 
-			this.add(prenomT); 
-
+			//Listener 
+			precedent.addMouseListener(new RetourListeContat());
+			
+			//Barre supérieur 
+			barreSuperieur.setLayout(new GridLayout(1, 3, 130, 0));
+			barreSuperieur.add(precedent); 
+			barreSuperieur.add(modifier); 
+			barreSuperieur.add(supprimer); 
+			modifier.setVisible(false);
+			supprimer.setVisible(false);
+			
+			//Partie Photo
+			titre.setHorizontalAlignment(titre.CENTER);
+			photo.setLayout(new BorderLayout());
+			photo.add(barreSuperieur, BorderLayout.NORTH);
+			photo.add(titre, BorderLayout.SOUTH);
+			photo.add(phtContact, BorderLayout.CENTER); 
+			titre.setVisible(false);
+			
+			//Partie formulaire
+			formulaire.setLayout(new GridLayout(4,2,-50,0));
+			formulaire.add(nomL); 
+			formulaire.add(nomT); 
+			formulaire.add(prenomL); 
+			formulaire.add(prenomT);  
+			formulaire.add(numtelL); 
+			formulaire.add(numTelT); 
+			formulaire.add(mailL); 
+			formulaire.add(mailT);
+			
+			
+			//Tous le panel 
+			this.setLayout(new GridLayout(3, 1,0,30));
+			this.add(photo);
+			this.add(formulaire); 	
+			this.add(sauvegarder); 
+			
+		}
+		
+		class RetourListeContat extends MouseAdapter
+		{
+			@Override
+			public void mouseClicked(MouseEvent arg0)
+			{
+				cl.show(cards, "Liste");
+			}
 		}
 		
 	}
@@ -102,11 +163,13 @@ public class AppliContact extends JPanel{
 		private String nom; 
 		private String prenom; 
 		private String numTelephone; 
+		private String mail; 
 		
-		public Contact(String nom, String prenom, String numTelephone) {
+		public Contact(String nom, String prenom, String numTelephone, String mail) {
 			this.nom = nom; 
 			this.prenom = prenom; 
 			this.numTelephone = numTelephone;
+			this.mail = mail; 
 		}
 
 		public String getNom() {
