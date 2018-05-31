ebff2e50401690f752fe3f2e1558c7a6147274da
Audrey <Audrey@DESKTOP-O3OQR6H>
2018-05-31 17:09:34 +0200

    31.05.2018 -> Memory début

--- src/AppliMemory/AppMemory.java
+++ src/AppliMemory/AppMemory.java
@@ -7,12 +7,21 @@
 
 package AppliMemory;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javafx.scene.text.Font;
 
 public class AppMemory extends JPanel {
 
+	//JLabel titre 
+	private JLabel titre = new JLabel("Memory"); 
+	
 	public AppMemory() {
 		
+		//Titre 
+		this.add(titre, BorderLayout.NORTH);
 	}
 	
 }
