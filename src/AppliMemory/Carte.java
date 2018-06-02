ddc5022cc2a3bd176ba4e4a37074669d0773763b
Audrey <Audrey@DESKTOP-O3OQR6H>
2018-06-02 10:07:37 +0200

    02.06.2018 -> Suite memo

--- src/AppliMemory/Carte.java
+++ src/AppliMemory/Carte.java
@@ -7,38 +7,67 @@
 
 package AppliMemory;
 
-
-import java.awt.Color;
+import java.awt.CardLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Carte extends JButton{
 	
-	private ImageIcon pays; 
-	private Carte paire; 
+	private int cptClic = 0; 
+	private int idCarte; 
+	private int idCartePartenaire; 
+	private ImageIcon pays;  
 	private ImageIcon couverture = new ImageIcon("ImageMemory/japan.png");
+	private CardLayout cl; 
+	private JPanel card; 
+	private MouseListener retournerCrt = new RetournerCarte(); 
 	
-	public Carte(ImageIcon pays) {
+	public Carte(ImageIcon pays, int idCarte, int idCartePartenaire) {
 		this.pays = pays;  
-		//this.paire = paire; 
+		this.idCarte = idCarte; 
+		this.idCartePartenaire = idCartePartenaire; 
 		
 		this.setSize(couverture.getIconWidth(), couverture.getIconWidth());
 		this.setPreferredSize(this.getSize());
 		this.setIcon(couverture);
-		this.addMouseListener(new RetournerCarte());
+		this.addMouseListener(retournerCrt);
 	}
 	
+	public MouseListener getRetournerCrt() {
+		return retournerCrt;
+	}
+
 	private void changerCouleurCarte() {
-		this.setIcon(pays);
-		
+		cptClic++; 
+		if (cptClic == 1) {
+			this.setIcon(pays);
+		}else{
+			this.setIcon(couverture);
+			cptClic = 0;
+		}
 	}
 	
-	public String toString() {
-		String s = pays.toString(); 
-		return s; 
+	public int getIdCartePartenaire() {
+		return idCartePartenaire;
+	}
+
+	public ImageIcon getPays() {
+		return pays;
+	}
+
+	public boolean equals(Carte crt) {
+		
+		if( (pays.toString()).equals(crt.getPays().toString()) ) {
+			return true; 
+		}
+		
+		return false; 
+		
 	}
 
 	class RetournerCarte extends MouseAdapter {
--- src/AppliMemory/EspaceJeu.java
+++ src/AppliMemory/EspaceJeu.java
@@ -3,6 +3,8 @@ package AppliMemory;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 
 import javax.swing.ImageIcon;
@@ -22,8 +24,11 @@ public class EspaceJeu extends JPanel {
 	private JPanel plateau = new JPanel(); 
 	
 	//Tableau de carte 
-	Carte[] tabCarte = new Carte[30]; 
+	private Carte[] tabCarte = new Carte[30]; 
 	
+	//Compteur 
+	private int compteurClic = 0; 
+
 	public EspaceJeu(CardLayout clMemo, JPanel cardMemo) {
 		this.clMemo = clMemo; 
 		this.cardMemo = cardMemo; 
@@ -40,14 +45,23 @@ public class EspaceJeu extends JPanel {
 		MelangerCarte();
 		
 		for(int i=0; i<tabCarte.length; i++) {
+			tabCarte[i].addMouseListener(new EnregistrerClic());
 			plateau.add(tabCarte[i]);
 		}
-		
+
 		this.add(plateau, BorderLayout.CENTER);
 		
 	}
 	
-	//Crée les cartes en leurs associant un pays. 
+	public int getCompteurClic() {
+		return compteurClic;
+	}
+
+	public void setCompteurClic(int compteurClic) {
+		this.compteurClic = compteurClic;
+	}
+	
+	//Crée les cartes en leurs associant un pays et les met dans un tab dans l'ordre 
 	public Carte[] attributionPaysCarte() {
 		
 		//Le fichier ou se trouve les img
@@ -61,14 +75,15 @@ public class EspaceJeu extends JPanel {
 		for(int i=0; i<30; i=i+2) {
 			//On attribue une image à la carte 
 			ImageIcon paysChoisi = new ImageIcon("ImageMemory/Pays/"+tabPays[cpt]); 
-			tabCarte[i] = new Carte(paysChoisi); 
-			tabCarte[i+1] = new Carte(paysChoisi);
+			tabCarte[i] = new Carte(paysChoisi, i ,(i+1)); 
+			tabCarte[i+1] = new Carte(paysChoisi, (i+1) ,i);
 			cpt++; 
 		}
 		
 		return tabCarte; 
 	}
 	
+	//Mélanger les cartes dans le désordre 
 	public void MelangerCarte() {
 		
 		int nb1; 
@@ -83,6 +98,36 @@ public class EspaceJeu extends JPanel {
 			tabCarte[nb2] = carteTempo; 
 		}
 		
+	}
+	
+	class EnregistrerClic extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			
+			Carte carte1 = null; 
+			Carte carte2 = null; 
+			
+			switch(compteurClic) {
+			case 0:
+				carte1 = (Carte) arg0.getSource();
+				System.out.println(carte1.getPays().toString());
+				compteurClic++; 
+				break;
+			case 1:
+				carte2 = (Carte) arg0.getSource(); 
+				System.out.println(carte2.getPays().toString());
+				/*if() {
+					carte1.removeMouseListener(carte1.getRetournerCrt());
+					carte2.removeMouseListener(carte2.getRetournerCrt());
+				}
+				break; */
+			//case 2:
+				
+			}
+			
+			
+			
+		}
 	}
 	
 }
